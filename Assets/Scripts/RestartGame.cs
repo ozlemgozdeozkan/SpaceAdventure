2c4e575d2528e603a16cf1c21ba0ceba07590815
Gözde <91250784+ozlemgozdeozkan@users.noreply.github.com>
2023-02-02 19:19:01 +0300

    Update RestartGame.cs

--- Assets/Scripts/RestartGame.cs
+++ Assets/Scripts/RestartGame.cs
@@ -6,24 +6,14 @@ using UnityEngine.SceneManagement;
 public class RestartGame : MonoBehaviour
 {
     public static bool isRestart = false;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+   
     public void restartGame()
     {   isRestart = true;
         PlayerController.coinCount = 0;
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);//Aktif sahneyi (son sahneyi) baştan yüklemeye yarar.
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
     public void quitGame()
     {
-        Application.Quit();// Oyundan çıkma komutudur.
+        Application.Quit();
     }
 }
